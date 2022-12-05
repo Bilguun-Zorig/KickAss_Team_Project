bb9443a1f0bbd4d59ce7980fc6b232be2b0022e6
pichatekij <110946180+pichatekij@users.noreply.github.com>
2022-12-05 12:48:39 -0500

    add test case 02 to KPI_Performance_Page_Pichate and modified KPIPerformancePageObjects and configAppText

--- src/test/java/projectTest/KPI_Performance_Page_Pichate.java
+++ src/test/java/projectTest/KPI_Performance_Page_Pichate.java
@@ -1,11 +1,10 @@
 package projectTest;
 
-import org.openqa.selenium.Keys;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import utilities.BaseClass;
 import utilities.CommonMethods;
 import utilities.ListenerAnalyzer;
 
@@ -37,5 +36,20 @@ public class KPI_Performance_Page_Pichate extends CommonMethods{
 		
 	}
 	
+	@Test(enabled=true)
+	public void test02EditKPI() {
+		kppo.clickEditButton(kppo.listOfKpi, "kiptx");
+		asserts(kppo.subHeader, "ek");
+		sendTextByConfigText(kppo.keyInputBox, "ekiptx");
+		click(kppo.saveButton);
+		asserts(kppo.pageHeader, "pf");
+	}
+	
+	@AfterMethod(enabled=true)
+	public void loggingOut() {
+		click(cpo.userDropdownMenu);
+		selection(cpo.userDropdownMenuItems, "logout");
+	}
+		
 	
 }
