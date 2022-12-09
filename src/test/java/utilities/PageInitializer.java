26f2cd7df9dbce3215bc8fa2cd830baeabd85c46
Alinnie26 <p.pornalin@gmail.com>
2022-12-08 22:42:47 -0500

    AlinTestCase

--- src/test/java/utilities/PageInitializer.java
+++ src/test/java/utilities/PageInitializer.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import pageObjects.CommonPageObjects;
 import pageObjects.HomePageObjects;
 import pageObjects.KPIPerformancePageObjects;
+import pageObjects.MyInfoPageObjects;
 import pageObjects.PIMPageObjects;
 
 public class PageInitializer extends BaseClass{
@@ -14,6 +15,7 @@ public class PageInitializer extends BaseClass{
 	public static CommonPageObjects cpo;
 	public static PIMPageObjects ppo;
 	public static KPIPerformancePageObjects kppo;
+	public static MyInfoPageObjects mipo;
 	
 	public static void initializer() {
 		wait = new WebDriverWait(BaseClass.getDriver(), Constants.Explicit_Wait_Time);
@@ -21,5 +23,6 @@ public class PageInitializer extends BaseClass{
 		cpo = new CommonPageObjects();
 		ppo = new PIMPageObjects();
 		kppo = new KPIPerformancePageObjects();
+	    mipo = new MyInfoPageObjects ();
 	}
 }
