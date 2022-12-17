3e84be2534d0ef9bacf3f307ce56e002d4ba022d
Bill <bilguun.zorig@yahoo.com>
2022-12-17 10:35:04 -0500

    update

--- src/test/java/projectTest/DirectoryTest.java
+++ src/test/java/projectTest/DirectoryTest.java
@@ -1,12 +1,10 @@
 package projectTest;
 
-import org.openqa.selenium.Keys;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import utilities.BaseClass;
 import utilities.CommonMethods;
 import utilities.ListenerAnalyzer;
 
