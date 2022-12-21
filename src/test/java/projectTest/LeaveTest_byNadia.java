12412b0e5779ae72a48f941781c8b9cd2639deb9
Bill <bilguun.zorig@yahoo.com>
2022-12-21 16:27:00 -0500

    some clean up

--- src/test/java/projectTest/LeaveTest_byNadia.java
+++ src/test/java/projectTest/LeaveTest_byNadia.java
@@ -1,6 +1,5 @@
 package projectTest;
 
-import org.bouncycastle.crypto.macs.CMac;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
--- src/test/java/projectTest/MyInfo_Page_Alin.java
+++ src/test/java/projectTest/MyInfo_Page_Alin.java
@@ -1,7 +1,6 @@
 package projectTest;
 
 
-import org.testng.annotations.Test;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
