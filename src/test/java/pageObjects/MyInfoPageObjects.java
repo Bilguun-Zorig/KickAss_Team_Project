0085660f2bfbdbf2db8dd73818b47eb58766edcb
Bill <bilguun.zorig@yahoo.com>
2022-12-22 21:36:09 -0500

    clean up

--- src/test/java/pageObjects/MyInfoPageObjects.java
+++ src/test/java/pageObjects/MyInfoPageObjects.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
 
 import utilities.BaseClass;
 import utilities.CommonMethods;
